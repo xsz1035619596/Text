eb4cb0e04234ff208e032a40c6c92df0155cf4d7
qhj <1035619596@qq.com>
2018-05-28 15:28:14 +0800

    qhj

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace WebApplication2.Controllers
     public class HomeController : Controller
     {
         /// <summary> 
-        /// 主ye   
+        /// 主页    
         /// </summary>
         /// <returns></returns>
         public ActionResult Index()
